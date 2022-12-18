cd0a5f46d08ec58adb194eceacf891ffc4b750d2
Dark_Soul <irina2002.nikolaeva@yandex.ru>
2022-12-18 19:22:04 +0300

    add navigation

---
+++ src/main/main.js
@@ -0,0 +1,20 @@
+import { getDir, up, cd, ls } from "../navigation/navigate.js";
+
+const processArg = process.argv[2];
+const username = processArg.slice(processArg.indexOf("=") + 1);
+console.log(`Welcome to the File Manager, ${username}!\n`);
+getDir();
+const readable = process.stdin;
+
+process.on("SIGINT", () => {
+    console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
+    process.exit(0);
+});
+readable.on("data", (chunk) => {
+    const result = chunk.toString();
+    const command = result.slice(0, result.indexOf(" ") || result.length - 1);
+    const args = result.slice(result.indexOf(" ") + 1).trim();
+    eval(`${command}(args)`);
+    getDir();
+    if (result.match(".exit")) process.emit("SIGINT");
+});
---
+++ src/navigation/navigate.js
@@ -0,0 +1,52 @@
+import path from "path";
+import { homedir } from "os";
+import { stat } from "fs/promises";
+import { readdir } from "fs";
+
+let currentDirectory = homedir();
+
+function getDir() {
+    console.log("You are currently in " + currentDirectory);
+}
+
+function up() {
+    if (currentDirectory != homedir()) {
+        currentDirectory = path.dirname(currentDirectory);
+    }
+}
+
+function cd(pathArg) {
+    if (path.isAbsolute(pathArg)) currentDirectory = pathArg;
+    else currentDirectory = path.join(currentDirectory, pathArg);
+}
+
+function ls() {
+    readdir(currentDirectory, async (err, files) => {
+        if (err) console.log("error");
+        else {
+            function itemSorting(item1, item2) {
+                if (item1.Type == "directory")
+                    if (item2.Type == "file") return -1;
+                    else return item1.Name.localeCompare(item2.Name);
+                else {
+                    if (item2.Type == "directory") return 1;
+                    else return item1.Name.localeCompare(item2.Name);
+                }
+            }
+
+            files = files.map(async (file) => {
+                let type = await stat(path.join(currentDirectory, file));
+                type = type.isDirectory() ? "directory" : "file";
+                const obj = {};
+                obj.Name = file;
+                obj.Type = type;
+                return obj;
+            });
+            Promise.all(files).then((result) =>
+                console.table(result.sort(itemSorting))
+            );
+        }
+    });
+}
+
+export { getDir, up, cd, ls };
